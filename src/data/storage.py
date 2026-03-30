58968301418659510776e2016dfb621df98344a2
Vladimir Herrera <vladimirherreram@gmail.com>
2026-03-29 20:22:07 -0500

    feat: se agrega listar libros2

--- src/data/storage.py
+++ src/data/storage.py
@@ -3,7 +3,7 @@ from models.Usuario import Usuario
 
 # Base de datos quemada para poder hacer pruebas..
 
-# Lista de libros vasia para agregar
+# Lista de libros vacia para agregar
 libros = []
 
 # Lista de usuarios
