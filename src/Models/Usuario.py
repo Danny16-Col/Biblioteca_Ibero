21027cb17748f861678a68f887c7722f5e09193e
Juan David Orozco <orozcoolmos77@gmail.com>
2026-04-01 14:15:30 -0500

    Se comentan algunas cosas faltantes

--- src/Models/Usuario.py
+++ src/Models/Usuario.py
@@ -1,6 +1,9 @@
 class Usuario:
+    #Clase que representa un usuario del sistema.
+    
     def __init__(self, id_usuario, nombre, usuario, contraseña):
-    #Juan estos son loos atributos que se necestian para crear un usuario
+        #Constructor de la clase usuario
+        
         self.__id_usuario = id_usuario
         self.__nombre = nombre
         self.__usuario = usuario
@@ -8,13 +11,17 @@ class Usuario:
 
     def get_id(self):
         return self.__id_usuario
+    #Retorna el ID del usuario
 
     def get_nombre(self):
         return self.__nombre
+    #Retorna el nombre del usuario
     
     def get_usuario(self):
         return self.__usuario
+    #Retorna el nombre de usuario
     
     def get_contraseña(self):
         return self.__contraseña
+    #Retorna la contraseña del usuario
 
--- src/controllers/usuario_controller.py
+++ src/controllers/usuario_controller.py
@@ -1,6 +1,28 @@
 from services.usuarios_service import registrar_usuario
-
-
+          
+def registro():
+    
+    #Solicita el nombre completo del usuario y elimina espacios innecesarios
+    nombre = input("Nombre completo: ").strip()
+    
+    #Solicita un nombre de usuario
+    usuario=input("Nuevo usuario: ").strip()
+    #Solicita una contraseña nueva
+    contraseña = input("Nueva contraseña: ").strip()
+ 
+ #Llama al servicio para registrar el usuario            
+    id_creado = registrar_usuario(nombre,usuario,contraseña)
+      
+      #Si se crea correctamente, el servicio retorna el ID del usuario       
+    if id_creado:
+        print(f"Usuario creado con ID: {id_creado}")
+        print("Usuario registrado con exito")
+        
+        #Si no, significa que el usuario ya existe
+    else:
+        print("Ese usuario ya existe")
+                    
+                    
 """""
   #Inicio de sesión de usuarios registrados          
 def login():
@@ -14,24 +36,6 @@ def login():
         menu_biblioteca()
     else:
         print("Credenciales incorrectas")
-"""""     
-#Registro para nuevos usuarios y se asigna un id               
-def registro():
-    
-    nombre = input("Nombre completo: ").strip()
-    usuario=input("Nuevo usuario: ").strip()
-    contraseña = input("Nueva contraseña: ").strip()
-             
-    id_creado = registrar_usuario(nombre,usuario,contraseña)
-             
-    if id_creado:
-        print(f"Usuario creado con ID: {id_creado}")
-        print("Usuario registrado con exito")
-        
-    else:
-        print("Ese usuario ya existe")
-                    
-                    
-
+""""" 
                         
                         
